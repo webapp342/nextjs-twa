1fc0c7e4b8d16395b35d652e1f8e616da485d4f5
19984646 <busetaktas45@gmail.com>
2024-12-06 10:53:01 +0330

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next';
 import { getLocale } from 'next-intl/server';
 
 import { Root } from '@/components/Root/Root';
-import { I18nProvider } from '@/core/i18n/provider';
 
 import '@telegram-apps/telegram-ui/dist/styles.css';
 import 'normalize.css/normalize.css';
@@ -20,11 +19,11 @@ export default async function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang={locale}>
     <body>
-      <I18nProvider>
+   
         <Root>
           {children}
         </Root>
-      </I18nProvider>
+     
     </body>
     </html>
   );
--- src/components/LocaleSwitcher/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher/LocaleSwitcher.tsx
@@ -1,25 +1,25 @@
 'use client';
 
 import { Select } from '@telegram-apps/telegram-ui';
-import { useLocale } from 'next-intl';
-import { FC } from 'react';
-
-import { localesMap } from '@/core/i18n/config';
-import { setLocale } from '@/core/i18n/locale';
-import { Locale } from '@/core/i18n/types';
+import { FC, useEffect, useState } from 'react';
+import { localesMap } from '@/core/i18n/config';  // Yerel dillerin listesi
+import { setLocale } from '@/core/i18n/locale';  // Locale'ı güncelleyen fonksiyon
 
 export const LocaleSwitcher: FC = () => {
-  const locale = useLocale();
+  const [locale, setLocaleState] = useState<string>('en'); // Başlangıç dilini 'en' olarak ayarlıyoruz
 
+  // Dil değişimi olduğunda locale'ı güncelliyoruz
   const onChange = (value: string) => {
-    const locale = value as Locale;
-    setLocale(locale);
+    setLocaleState(value);  // Dil değişimini local state'e kaydediyoruz
+    setLocale(value);  // Dil değişimini cookie ya da localStorage'a kaydediyoruz
   };
 
   return (
     <Select value={locale} onChange={({ target }) => onChange(target.value)}>
       {localesMap.map((locale) => (
-        <option key={locale.key} value={locale.key}>{locale.title}</option>
+        <option key={locale.key} value={locale.key}>
+          {locale.title}
+        </option>
       ))}
     </Select>
   );
