eeb6c7bc0ee421af933a544cc2acba303845f379
19984646 <busetaktas45@gmail.com>
2024-12-06 10:32:41 +0330

    updated

--- src/core/i18n/provider.tsx
+++ src/core/i18n/provider.tsx
@@ -1,31 +1,15 @@
-'use client'; // Add this line to mark the component as a Client Component
-
 import { NextIntlClientProvider } from "next-intl";
-import { useEffect, useState } from "react";
 import { getMessages } from "next-intl/server";
+import React from "react";
 
-// Define MessagesType to match the structure of the returned messages
-type MessagesType = Record<string, string>;
+import { timeZone } from "./config";
 
-const I18nProvider: React.FC<React.PropsWithChildren> = ({ children }) => {
-  const [messages, setMessages] = useState<MessagesType | null>(null);
-
-  useEffect(() => {
-    // Fetch the messages asynchronously
-    const fetchMessages = async () => {
-      const fetchedMessages = await getMessages();
-      setMessages(fetchedMessages as MessagesType); // Explicitly cast the fetched messages to the expected type
-    };
-
-    fetchMessages();
-  }, []);
-
-  if (!messages) {
-    return <div>Loading...</div>; // Loading state while messages are being fetched
-  }
-
+const I18nProvider: React.FC<React.PropsWithChildren> = async ({
+  children,
+}) => {
+  const messages = await getMessages();
   return (
-    <NextIntlClientProvider messages={messages}>
+    <NextIntlClientProvider messages={messages} timeZone={timeZone}>
       {children}
     </NextIntlClientProvider>
   );
