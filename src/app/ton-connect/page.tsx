ca247cabd53707b7688c43aa47f0fbe1912b0e74
19984646 <busetaktas45@gmail.com>
2024-12-06 10:10:51 +0330

    updated

--- src/app/ton-connect/page.tsx
+++ src/app/ton-connect/page.tsx
@@ -1,28 +1,19 @@
 'use client';
 
-
-// Import the necessary styles globally
-import '@telegram-apps/telegram-ui/dist/styles.css';
-
-// Import components from the library
 import { AppRoot, Cell, List, Section } from '@telegram-apps/telegram-ui';
 
-// Example data for rendering list cells
 const cellsTexts = ['Chat Settings', 'Data and Storage', 'Devices'];
 
-export const App = () => (
+const page = () => (
   <AppRoot>
-    {/* List component to display a collection of items */}
     <List>
-      {/* Section component to group items within the list */}
       <Section header="Header for the section" footer="Footer for the section">
-        {/* Mapping through the cells data to render Cell components */}
         {cellsTexts.map((cellText, index) => (
-          <Cell key={index}>
-            {cellText}
-          </Cell>
+          <Cell key={index}>{cellText}</Cell>
         ))}
       </Section>
     </List>
   </AppRoot>
 );
+
+export default page;
