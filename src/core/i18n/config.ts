906ad5642cdf3bcb508c99314a9a161067a9ef5e
19984646 <busetaktas45@gmail.com>
2024-12-06 07:38:25 +0330

    updated

--- src/core/i18n/config.ts
+++ src/core/i18n/config.ts
@@ -2,9 +2,10 @@ export const defaultLocale = 'en';
 
 export const timeZone = 'Europe/Amsterdam';
 
-export const locales = [defaultLocale, 'ru'] as const;
+export const locales = [defaultLocale, 'ru', 'tr'] as const;
 
 export const localesMap = [
   { key: 'en', title: 'English' },
   { key: 'ru', title: 'Русский' },
+  { key: 'tr', title: 'Türkçe' },
 ];
